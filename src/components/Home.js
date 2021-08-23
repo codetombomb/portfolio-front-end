7b95494ff6294ff39a1824e6229a8871cd497cc6
Tom Tobar <tomas.tobar@flatironschool.com>
2021-08-22 19:22:38 -0500

    add basic styles to home and about components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import TomLogo from '../tom_logo.svg'
 
 function Home(props) {
     return (
-        <div>
-            This is Home
+        <div id="home">
+            <img src={TomLogo} />
         </div>
     );
 }
 
 export default Home;
+
+//Diplay the tom logo
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,9 @@
 import '../App.css';
 import Portfolio from '../containers/Portfolio';
-import Nav from '../components/Nav'
 
 function App() {
   return (
     <div className="App">
-        <Nav />
         <Portfolio />
     </div>
   );
