022fe765a370201a28b65babf7cebf9415a22948
Tom Tobar <tomas.tobar@flatironschool.com>
2021-08-24 23:16:14 -0500

    add change work img on click

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Works(props) {
+    const [count, setCount] = useState(0)
+    const [displayed, setDisplayed] = useState("")
+    const img = `process.env.PUBLIC_URL../../worksImgs/${displayed}.png`
+
+    useEffect(() => {
+        setDisplayed(props.works[count])
+    })
+
     const handleClick = (e) => {
         console.log("moving img")
+        if (count < props.works.length - 1){
+            setCount(count + 1)
+        } else {
+            setCount(0)
+        }
     }
 
     return (
         <div id="works">
             <div id="works-container">
-                {props.works.map(pic => {
-                    const img = `process.env.PUBLIC_URL../../worksImgs/${pic}.png`
-                    return <img key={pic} alt="Works Ad" onClick={handleClick} src={img} />
-                })}
+                <img key={count} id={displayed} alt="Works Ad" onClick={handleClick} src={img} />
             </div>
         </div>
     );
