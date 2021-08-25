8a27de103a2e783a87eb315fd24f2c274e9d0495
Tom Tobar <tomas.tobar@flatironschool.com>
2021-08-24 22:15:51 -0500

    render works images

--- src/worksImages.js
+++ src/worksImages.js
@@ -1,7 +1,3 @@
-const worksImages =  {
-    covidChaos: '/static/media/covid_chaos.7718c604.svg',
-    jeopardy: '/static/media/jeopardy.svg',
-    reacTarot: '/static/media/reactarot.svg'
-}
+const worksImages = ["jeopardy", "covid_chaos", "reacTarot"]
 
 export default worksImages;
