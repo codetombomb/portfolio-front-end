d3fa4ce34f2002238b0fc04ba969cd15e2f1ff5a
Tom Tobar <tomas.tobar@flatironschool.com>
2021-08-25 12:37:21 -0500

    add connect component. Replace covid chaos ad img.

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <div id="contact">
+            Hello from Contact!
+        </div>
+    );
+}
+
+export default Contact;
