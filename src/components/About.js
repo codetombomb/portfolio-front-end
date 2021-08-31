bd92ec84f5554d67e7bcffdba5ddbe02ca0774f8
Tom Tobar <tomas.tobar@flatironschool.com>
2021-08-31 14:13:47 -0500

    remove unused code

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,8 @@ function About(props) {
     const overlayBlock = useRef();
     const bdayDiv = useRef();
     const infoDiv = useRef();
+    const bdayTitleH1 = useRef();
+    const bdayTitleH2 = useRef();
 
 
     function animateCreep(e) {
@@ -20,17 +22,29 @@ function About(props) {
         gsap.to(overlayBlock.current, {
             display: "block"
         })
+        gsap.to(bdayTitleH1.current, {
+            display: "block"
+        })
+        gsap.to(bdayTitleH2.current, {
+            display: "block"
+        })
         gsap.from(overlayBlock.current, {
             y: -150,
             opacity: 0,
             duration: .5,
             delay: .5
         })
-        gsap.from(bdayDiv.current, {
+        gsap.from(bdayTitleH1.current, {
             x: 50,
             opacity: 0,
             duration: .8,
-            delay: .5
+            delay: .2
+        })
+        gsap.from(bdayTitleH2.current, {
+            x: 50,
+            opacity: 0,
+            duration: .8,
+            delay: .4
         })
         gsap.from(infoDiv.current, {
             opacity: 0,
@@ -45,8 +59,8 @@ function About(props) {
             <div id="creep-pic-wrapper">
                 <div id="info-pic-container">
                     <div ref={bdayDiv} id="bday">
-                        <h1>85</h1>
-                        <h2>0907</h2>
+                        <h1 ref={bdayTitleH1}>85</h1>
+                        <h2 ref={bdayTitleH2}>0907</h2>
                     </div>
                     <div ref={infoDiv} style={{display: 'block'}} id="info-container">
                         {props.attributes.map((attr) => (<p key={attr}>{attr}</p>))}
