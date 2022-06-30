d654a271975e12b61ff5677f20540b02e44c69d0
Tom Tobar <cocdetombomb@gmail.com>
2022-06-29 23:37:28 -0500

    add styles to works

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,34 +7,14 @@ import projectData from '../../projectData';
 import React, {useState} from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom'
 
-const colors = {
-  green: "#D2DF35",
-  orange: "#FEA100",
-  purple: "#4541FF",
-  aqua: "#1CAA9D"
-}
-
 const buttons = [
   {type: "Works"},
-  {type: "About"},
   {type: "Contact"}
 ]
 
 function App() {
   const [page, setPage] = useState('/')
   const navigate = useNavigate()
-
-  const getColor = () => {
-    if(page === '/') {
-      return colors.green
-    } else if (page === 'about'){
-      return colors.purple
-    } else if (page === 'works'){
-      return colors.aqua
-    } else{
-      return colors.orange
-    }
-  }
 
   const onHandleClick = (e) => {
     setPage(e.target.textContent.toLowerCase())
@@ -43,7 +23,7 @@ function App() {
   }
 
   return (
-    <div style={{backgroundColor: getColor()}}className="App">
+    <div className="App">
         <Routes>
           <Route path='/' element={<Portfolio buttons={buttons} handleClick={onHandleClick}/>} />
           <Route path='/about' element={<About/>} />
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./containers/App/App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
