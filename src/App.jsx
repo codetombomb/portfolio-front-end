495439c94b98e78502e9da0092ad4b0144f02981
Tom Tobar <codetombomb@gmail.com>
2023-10-26 12:35:36 -0500

    add vite app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import Cube from "./components/Cube/Cube";
-import { OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
-    <Canvas style={{width: "100vw",height: "75vh", backgroundColor: "black"}}>
-      <ambientLight />
-      <spotLight position={[3, 3, 3]} angle={0.3} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Cube position={[0, 0, 0]} />
-      <OrbitControls /> 
-    </Canvas>
+    <h2>Hello</h2>
   );
 }
 
 export default App;
-
-
-// OrbitControls allow the scroll zoom 
