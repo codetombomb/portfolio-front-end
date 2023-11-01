0ad8ec67517fd9ea3b311d0c8957634296e4df53
Tom Tobar <codetombomb@gmail.com>
2023-10-31 20:27:28 -0500

    add navbar and initial styles

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={`${styles.navbar} flex`}>
+        <div className={`${styles.navLogo} grid grid-center`}>Tom Tobar</div>
+        <ul className={styles.navLinks}>
+            <li>about</li>
+            <li>github</li>
+            <li>linkedin</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
