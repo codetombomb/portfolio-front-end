a3845cf053f1f661a8e26a1a575c7751c785c9e6
Tom Tobar <codetombomb@gmail.com>
2023-11-01 21:34:44 -0500

    refactor out data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import AboutSection from "./components/AboutSection";
 import TopSection from "./components/TopSection";
 import WorksSection from "./components/WorksSection";
 
+import devData from './data'
+
 function App() {
+  
   return (
     <>
-      <TopSection />
-      <AboutSection />
-      <WorksSection />
+      <TopSection topSectionData={devData.topSection} />
+      <AboutSection aboutSectionData={devData.aboutSection} />
+      <WorksSection worksSectionData={devData.worksSection}/>
     </>
   );
 }
--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import style from './styles.module.css'
+import style from "./styles.module.css";
 
 import SectionTitle from "../SectionTitle";
 import SectionDescription from "../SectionDescription";
 import SectionCallToAction from "../SectionCallToAction";
 
-const AboutSection = () => {
+const AboutSection = ({ aboutSectionData }) => {
+  const { title, description, callToAction } = aboutSectionData;
   return (
     <section className={style.aboutSection}>
-      <SectionTitle title="about" color="--primary-light" />
-      <SectionDescription
-        text="Hi, I'm Tom and I am obsessed with code! For the passed two years, I have been teaching Full-Stack Web Development at Flatiron School. I love crafting designs and then breathing life into them with code tools."
-        color="--secondary-light"
-      />
-      <SectionCallToAction text="Learn More!" color="--primary-light" />
+      <SectionTitle title={title.text} color={title.color} />
+      <SectionDescription text={description.text} color={description.color} />
+      <SectionCallToAction text={callToAction.text} color={callToAction.color} />
     </section>
   );
 };
