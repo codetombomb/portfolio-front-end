8e2f47aabbc1bfa3e9a63179c7f882fb713e8e2b
Tom Tobar <codetombomb@gmail.com>
2023-11-01 20:08:02 -0500

    add works section; add responsive font size

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -7,14 +7,14 @@ import SectionCallToAction from "../SectionCallToAction";
 
 const AboutSection = () => {
   return (
-    <div className={style.aboutSection}>
-      <SectionTitle title={"about"} color="--primary-light" />
+    <section className={style.aboutSection}>
+      <SectionTitle title="about" color="--primary-light" />
       <SectionDescription
         text="Hi, I'm Tom and I am obsessed with code! For the passed two years, I have been teaching Full-Stack Web Development at Flatiron School. I love crafting designs and then breathing life into them with code tools."
         color="--secondary-light"
       />
       <SectionCallToAction text="Learn More!" color="--primary-light" />
-    </div>
+    </section>
   );
 };
 
