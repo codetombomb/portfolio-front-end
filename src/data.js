0947d6cc482d5451a92cb9309baf435c8078c060
Tom Tobar <codetombomb@gmail.com>
2023-11-01 21:34:44 -0500

    refactor out data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default {
+  topSection: {
+    title: {
+      text: "code tombomb",
+      color: "--primary-dark",
+    },
+    description: {
+      text: "Awakens designs with a sequence of characters",
+      color: "--primary-dark",
+    },
+    callToAction: {
+      text: "Let's Chat!",
+      color: "--primary-dark",
+    },
+    heroImage: {
+      source: "../../tom-image.png",
+    },
+  },
+  aboutSection: {
+    title: { text: "about", color: "--primary-light" },
+    description: {
+      text: "Hi, I'm Tom and I am obsessed with code! For the passed two years, I have been teaching Full-Stack Web Development at Flatiron School. I love crafting designs and then breathing life into them with code tools.",
+      color: "--secondary-light"
+    },
+    callToAction: {
+        text: "Learn More!",
+        color: "--primary-light",
+      }
+  },
+  worksSection: {
+    title: {text: "works", color: "--primary-lighter"},
+    description:
+      {text: "Here are some of my projects and blogs. More to come in the near future!", color: "--secondary-light"},
+  },
+};
