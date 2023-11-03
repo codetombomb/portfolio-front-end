85272e6a5e60f57fa8a1da4bc3b84ea296f5c171
Tom Tobar <codetombomb@gmail.com>
2023-11-03 16:41:17 -0500

    add mobile menu on hamburger click

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,6 +3,7 @@ import styles from "./styles.module.css";
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     window.addEventListener("resize", resize);
@@ -17,15 +18,34 @@ const Navbar = () => {
     }
   };
 
+  const toggleShowMenu = () => setShowMenu((previous) => !previous);
+
   return (
     <div className={`${styles.navbar} flex`}>
       <div className={`${styles.navLogo} grid grid-center`}>codetombomb</div>
       {isMobile ? (
-        <div className={styles.hamburgerMenu}>
-          <div className={`${styles.hamburgerSlice} ${styles.hamburgerSliceTop} `}></div>
-          <div className={`${styles.hamburgerSlice} ${styles.hamburgerSliceMid} `}></div>
-          <div className={`${styles.hamburgerSlice} ${styles.hamburgerSliceBottom} `}></div>
-        </div>
+        <>
+          <div className={`${styles.hamburgerMenu}`} onClick={toggleShowMenu}>
+            <div
+              className={`${styles.hamburgerSlice} ${showMenu ? styles.hamburgerSliceTop : null}`}
+            ></div>
+            <div
+              className={`${styles.hamburgerSlice} ${showMenu ? styles.hamburgerSliceMid : null}`}
+            ></div>
+            <div
+              className={`${styles.hamburgerSlice} ${showMenu ? styles.hamburgerSliceBottom : null}`}
+            ></div>
+          </div>
+          {showMenu && (
+            <div className={styles.mobileMenu}>
+              <ul className={styles.mobileNavLinks}>
+                <li>about</li>
+                <li>github</li>
+                <li>linkedin</li>
+              </ul>
+            </div>
+          )}
+        </>
       ) : (
         <ul className={styles.navLinks}>
           <li>about</li>
