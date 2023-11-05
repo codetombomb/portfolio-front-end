6de8259c1981390478f6a66bde5b97e14a6404b2
Tom Tobar <codetombomb@gmail.com>
2023-11-05 17:04:35 -0600

    add chat box to client; add chat server;

--- src/components/HeroImage/index.jsx
+++
@@ -1,9 +0,0 @@
-import style from './styles.module.css'
-
-const HeroImage = ({imageSrc}) => {
-  return (
-    <img className={style.heroImage} src={imageSrc} alt="Head shot of smiling developer, Tom Tobar" />
-  )
-}
-
-export default HeroImage
