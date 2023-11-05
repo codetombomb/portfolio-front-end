8827866c1fcef3ac68c0e451d676e03106a8da93
Tom Tobar <codetombomb@gmail.com>
2023-11-05 17:04:35 -0600

    add chat box to client; add chat server;

--- src/components/SectionDescription/index.jsx
+++
@@ -1,8 +0,0 @@
-import style from './styles.module.css'
-const SectionDescription = ({text, color}) => {
-  return (
-    <p className={style.sectionDescription} style={{color: color ? `var(${color})` : null}}>{text}</p>
-  )
-}
-
-export default SectionDescription
