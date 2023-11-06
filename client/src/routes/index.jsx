6200b06ad14303d498e192e8e079c423d449f3de
Tom Tobar <codetombomb@gmail.com>
2023-11-06 09:11:39 -0600

    add admin login page

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,18 +1,31 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import React from "react";
+import React, { useState } from "react";
 import App from "../App";
+import AdminLogin from "../pages/AdminLogin";
 
 const AppRoutes = () => {
   /** Future Feature -> Authorized ? Protected Routes + Public : Public only */
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+
+  const onLoginSuccess = (user) => {
+    setCurrentUser(user);
+    setIsAdmin(true);
+  };
 
   const element = createBrowserRouter([
     {
       path: "/",
-      element: <App />,
+      element: <App currentUser={currentUser} isAdmin={isAdmin} />,
+    },
+    {
+      path: "/admin",
+      element: <AdminLogin onLoginSuccess={onLoginSuccess} />,
     },
   ]);
-  
+
   return <RouterProvider router={element} />;
 };
 
