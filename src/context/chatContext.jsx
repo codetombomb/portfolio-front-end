eeeb58b1cea0d61aaa097e268a15ecdbea70b38d
Tomas Tobar <codetombomb@gmail.com>
2024-02-07 01:15:34 -0600

    persist device token

--- src/context/chatContext.jsx
+++ src/context/chatContext.jsx
@@ -19,7 +19,7 @@ const ChatProvider = ({ children }) => {
   const [currentAdmin, setCurrentAdmin] = useState({})
   const [chatTime, setChatTime] = useState("")
   const [isAdmin, setIsAdmin] = useState(false)
-  const [deviceToken, setDeviceToken] = useState(null)
+  const [deviceTokenId, setDeviceTokenId] = useState(null)
   const [currentChat, setCurrentChat] = useState({
     visitor_id: null,
     admin_id: null,
@@ -122,8 +122,8 @@ const ChatProvider = ({ children }) => {
         isAdmin,
         setIsAdmin,
         onAdminLogin,
-        deviceToken,
-        setDeviceToken,
+        deviceTokenId,
+        setDeviceTokenId,
         chatTime
       }}
     >
