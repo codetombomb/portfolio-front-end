036857f919012640b54743ddeb49d6535620cf8c
Tomas Tobar <codetombomb@gmail.com>
2024-02-09 12:56:44 -0600

    add gsap animations

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -4,13 +4,18 @@ import style from "./styles.module.css";
 import SectionTitle from "../SectionTitle";
 import SectionDescription from "../SectionDescription";
 import SectionCallToAction from "../SectionCallToAction";
+import TextAnimationWrapper from "../TextAnimationWrapper";
 
 const AboutSection = ({ aboutSectionData }) => {
   const { title, description, callToAction } = aboutSectionData;
   return (
     <section className={style.aboutSection}>
-      <SectionTitle title={title.text} color={title.color} />
-      <SectionDescription text={description.text} color={description.color} />
+      <TextAnimationWrapper delay={.6} duration={.5}>
+        <SectionTitle title={title.text} color={title.color} />
+      </TextAnimationWrapper>
+      <TextAnimationWrapper delay={.9} duration={.4}>
+        <SectionDescription text={description.text} color={description.color} />
+      </TextAnimationWrapper>
       <SectionCallToAction text={callToAction.text} color={callToAction.color} />
     </section>
   );
