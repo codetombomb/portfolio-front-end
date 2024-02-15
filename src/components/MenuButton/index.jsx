f48c498fdf421ac1bf3cf0323a37f503ee9a932f
Tomas Tobar <codetombomb@gmail.com>
2024-02-15 10:32:45 -0600

    debug menu btn slices

--- src/components/MenuButton/index.jsx
+++ src/components/MenuButton/index.jsx
@@ -14,12 +14,12 @@ const MenuButton = ({clicked, handleMenuBtnClick}) => {
     const sliceBottomRef = useRef(null)
 
     useGSAP(() => {
-        const wrapperMidPoint = wrapperRef.current.offsetHeight / 2
+        const wrapperMidPoint = wrapperRef.current.offsetHeight / 2 + 4
         if (clicked) {
             gsap.fromTo(menuBtnRef.current, { opacity: 0}, {opacity:1, delay: .3})
-            gsap.to(sliceTopRef.current, { backgroundColor: "var(--primary-dark)", rotate: 45, translateY: wrapperMidPoint / 2 })
+            gsap.to(sliceTopRef.current, { backgroundColor: "var(--primary-dark)", rotate: 45, translateY: (wrapperMidPoint)  / 2 })
             gsap.to(sliceMidRef.current, { backgroundColor: "var(--primary-dark)", opacity: 0 })
-            gsap.to(sliceBottomRef.current, { backgroundColor: "var(--primary-dark)", rotate: -45, translateY: -wrapperMidPoint / 2 })
+            gsap.to(sliceBottomRef.current, { backgroundColor: "var(--primary-dark)", rotate: -45, translateY: (-wrapperMidPoint) / 2 })
             gsap.to(backLayerRef.current, {borderColor: "var(--primary-light)"})
             gsap.to(midLayerRef.current, {borderColor: "var(--primary-light)"})
             gsap.to(frontLayerRef.current, {backgroundColor: "var(--primary-light)"})
@@ -41,10 +41,10 @@ const MenuButton = ({clicked, handleMenuBtnClick}) => {
             <div className={style.backLayer} ref={backLayerRef}></div>
             <div className={style.midLayer} ref={midLayerRef}></div>
             <div className={style.frontLayer} ref={frontLayerRef}>
-                <div ref={wrapperRef}>
-                    <div className={`${style.slice} ${style.sliceTop}`} ref={sliceTopRef}></div>
-                    <div className={`${style.slice}  ${style.sliceMid}`} ref={sliceMidRef}></div>
-                    <div className={`${style.slice}  ${style.sliceBottom}`} ref={sliceBottomRef}></div>
+                <div className={style.burgerWrapper} ref={wrapperRef}>
+                    <div className={`${style.slice}`} ref={sliceTopRef}></div>
+                    <div className={`${style.slice}`} ref={sliceMidRef}></div>
+                    <div className={`${style.slice}`} ref={sliceBottomRef}></div>
                 </div>
             </div>
         </div>
