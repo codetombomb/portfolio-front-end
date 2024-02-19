c64263e9d817dc629908a3ed15867bc3598c2365
Tomas Tobar <codetombomb@gmail.com>
2024-02-19 11:03:29 -0600

    add responsive slime

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -4,8 +4,7 @@ import style from "./styles.module.css";
 import SectionTitle from "../SectionTitle";
 import SectionDescription from "../SectionDescription";
 import TextAnimationWrapper from "../TextAnimationWrapper";
-import slime from "../../assets/slime.svg"
-import tomInSun from "../../assets/tom-in-sun.svg"
+import tomInSun from "../../assets/tom-in-sun.svg";
 import { v4 as uuidv4 } from "uuid";
 
 const AboutSection = ({ aboutSectionData }) => {
@@ -16,28 +15,25 @@ const AboutSection = ({ aboutSectionData }) => {
       return (
         <div className={style.aboutDesc} key={uuidv4()}>
           <SectionTitle title={desc.title.text} color={desc.title.color} />
-          <SectionDescription text={desc.description.text} color={desc.description.color} />
+          <SectionDescription
+            text={desc.description.text}
+            color={desc.description.color}
+          />
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
   return (
     <section className={style.aboutSection}>
-      <div className={style.slimeContainer}>
-        <img className={style.slime} src={slime} alt="slime" />
-      </div>
       <section className={style.aboutMain}>
-        <article className={style.headline}>
-          {renderDescriptions()}
-        </article>
+        <article className={style.headline}>{renderDescriptions()}</article>
         <div className={style.tomInSunWrapper}>
           <img className={style.tomInSun} src={tomInSun}></img>
         </div>
-
       </section>
     </section>
-  )
+  );
 };
 
 export default AboutSection;
