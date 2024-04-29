da99f96cb5dc6aaaa0273183257421281a52e040
Tomas Tobar <codetombomb@gmail.com>
2024-04-29 17:24:04 -0500

    init new vite app

--- src/components/SectionSubTitle/index.jsx
+++
@@ -1,9 +0,0 @@
-import style from './styles.module.css'
-const SectionSubTitle = ({title, color, padding}) => {
-
-  return (
-    <h3 className={style.sectionSubTitle} style={{color: color ? `var(${color})` : null, padding: padding ? padding : null}}>{title}</h3>
-  )
-}
-
-export default SectionSubTitle
