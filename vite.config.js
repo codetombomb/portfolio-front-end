4695af7aa060cc3f6eaa0054c8abbe6459462d3e
Tomas Tobar <codetombomb@gmail.com>
2024-04-29 17:24:04 -0500

    init new vite app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { qrcode } from "vite-plugin-qrcode";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), qrcode()],
-});
+  plugins: [react()],
+})
