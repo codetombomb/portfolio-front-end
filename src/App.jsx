d3a237116a21717785067c8c935d1520f948e697
Tomas Tobar <codetombomb@gmail.com>
2024-05-02 11:04:43 -0500

    add about section mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <>
       <Home />
-      <h1>About</h1>
+      <About />
       <h1>Works Title</h1>
       <h1>Work 1</h1>
       <h1>Work 2</h1>
