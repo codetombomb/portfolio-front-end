0eeaca5979b96c7866832f8e423188f8b7bc6b7a
Tomas Tobar <codetombomb@gmail.com>
2024-05-29 15:00:20 -0500

    re-work background image for responsiveness

--- src/sections/PyCliGpt/PyCliGpt.jsx
+++ src/sections/PyCliGpt/PyCliGpt.jsx
@@ -9,6 +9,7 @@ function PyCliGpt() {
     width: window.innerWidth,
     height: window.innerHeight,
   });
+
   useEffect(() => {
     const handleResize = () => {
       setWindowSize({
@@ -25,26 +26,37 @@ function PyCliGpt() {
   }, []);
 
   return (
-    <section className={`section ${styles.workTwo} `}>
-      <h1>py-cli</h1>
-      <h1>gpt</h1>
-      <div className={styles.bgImgWrapper}>
-        <div ref={robotLazerRef} className={styles.robotLazerWrapper}>
-          <img
-            className={styles.robotImg}
-            src="./robot.png"
-            alt="Purple robot shooting an surf green beam out of its eyes"
+    <section className={`section ${styles.workTwo}`}>
+      <h1 className={styles.workTwoTitle}>py-cli</h1>
+      <h1 className={styles.workTwoTitle}>gpt</h1>
+      <div className={styles.robotImgContainer}>
+        <img
+          className={styles.zebraPrint}
+          src="./zebra-print.webp"
+          alt="Zebra print background"
+        />
+        <img
+          className={styles.robotImg}
+          src="./robot-laser.webp"
+          alt="Purple robot shooting a surf green beam out of its eyes"
+        />
+        <div
+          className={`${styles.workTwoDescription} flex-column flex-space-between`}
+        >
+          <h1 className={styles.w2mainTitle}>AI Powered Command Line Chat</h1>
+          <p className={styles.w2mainDescription}>
+            PyCli-GPT is a Python CLI using OpenAI's GPT to answer questions and
+            assist with tasks. Clone, set up your API key, and start exploring!
+          </p>
+          <SocialIcon
+            iconName="github"
+            icon={githubIcon}
+            handleIconClick={() =>
+              window.open("https://github.com/codetombomb/cli-gpt", "_blank")
+            }
           />
-          <div className={styles.eyeDot}></div>
         </div>
       </div>
-      <SocialIcon
-        iconName="github"
-        icon={githubIcon}
-        handleIconClick={() =>
-          window.open("https://github.com/codetombomb/cli-gpt", "_blank")
-        }
-      />
     </section>
   );
 }
